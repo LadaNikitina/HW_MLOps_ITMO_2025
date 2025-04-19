c3d74f05d0c00ad8cc053140838dd1170cbe415a
Daria Ledneva <ledneva@dgx1.ipavlov.mipt.ru>
2025-04-19 23:04:01 +0300

    Fix device for Mac

--- src/embeddings.py
+++ src/embeddings.py
@@ -3,7 +3,12 @@ import numpy as np
 from tqdm import tqdm
 from transformers import AutoTokenizer, BertConfig, AutoModel
 
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+if torch.backends.mps.is_available():
+    device = torch.device("mps")
+elif torch.cuda.is_available():
+    device = torch.device("cuda")
+else:
+    device = torch.device("cpu")
 
 def get_dnabert2_embeddings(dataloader, tokenizer, model, pooling="mean"):
     embeddings = []
